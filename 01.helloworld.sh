41b0cf143a6a59e8e884103d865a03616339390e
Mehar Sai <meharsairavikanti@gmail.com>
2026-04-25 15:30:41 +0530

    Update 01.helloworld.sh

--- 01.helloworld.sh
+++ 01.helloworld.sh
@@ -1,6 +1,6 @@
 #1/bin/bash     
 
-#shiband this is the kind of interoreter before running it checks errors releated to shell scripting
+#shibang this is the kind of interoreter before running it checks errors releated to shell scripting
 
 
 echo "Hello-World"
