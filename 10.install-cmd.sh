37f401a30ff10f3746714e49a710f28d562910e2
meharsai12 <meharsairavikanti@gmail.com>
2026-04-27 14:10:17 +0530

    Args through

--- 10.install-cmd.sh
+++ 10.install-cmd.sh
@@ -6,7 +6,8 @@ USERID=$(id-u)
 if [ $USERID -ne 0 ]
 then
 
-echo " Ypu don't have root access to perform"
+echo " Error :; You don't have root access to perform"
+exit 1
 
 else
    echo "You have root aces you can perform"
