7f02b401e130886e68ad105d436434fedaea9587
meharsai12 <meharsairavikanti@gmail.com>
2026-04-27 13:25:53 +0530

    Args through

---
+++ 04.variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1 #passing valuues as arguments while running script
+PERSON2=$2 #passing valuues as arguments while running script
+
+
+
+echo "PERSON1 is good "
+echo "PERSON2 is brother of PERSON1"
