86a0dc796cce38bee233d3dd8ecbac0389733f99
meharsai12 <meharsairavikanti@gmail.com>
2026-04-27 13:38:16 +0530

    Args through

---
+++ 06.data-types.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+NUMBER1=100
+NUMBER2=200
+
+
+
+TIMESTAMP=$(date)
+
+echo "The script executed at :: $TIMESTAMP "
+
+SUM=$(($NUMBER1+$NUMBER2))
+
+
+echo "The sum od two numbers is :: $SUM"
