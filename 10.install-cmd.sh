1fff6046949b3cdc5afc0b79b22fe120675094e2
meharsai12 <meharsairavikanti@gmail.com>
2026-04-27 14:29:28 +0530

    Args through

--- 10.install-cmd.sh
+++ 10.install-cmd.sh
@@ -12,3 +12,24 @@ exit 1
 else
    echo "You have root aces you can perform"
 fi
+
+
+dnf list installed mysql
+
+#If the response is 0 by running command $?= The output of previous command 
+
+if [ $? -ne 0 ]
+then 
+    echo "Mysql i snot installed going to install it "
+    dnf install mysql
+    if[ $? eq 0 ]
+    then 
+        echo " My sql installatrion is succesful"
+    else
+        echo "Error Mysql installation is failure"
+        exit 1
+    fi
+else 
+    echo "My sql is already installed "Nothing to do ""
+
+fi
