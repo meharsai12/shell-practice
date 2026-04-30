505bfe8e40f2649c42ec2950a2f978a2d311a11f
meharsai12 <meharsairavikanti@gmail.com>
2026-04-30 13:36:33 +0530

    Args through

--- 15.functions.sh
+++ 15.functions.sh
@@ -11,23 +11,23 @@ LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 PACKAGES=("mysql" "python" "nginx" "httpd")
 
 mkdir -p $LOGS_FOLDER
-echo "Script started executing at: $(date)" | tee -a $LOG_FILE
+echo "Script started executing at: $(date)" 
 
 if [ $USERID -ne 0 ]
 then
-    echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
+    echo -e "$R ERROR:: Please run this script with root access $N" 
     exit 1 #give other than 0 upto 127
 else
-    echo "You are running with root access" | tee -a $LOG_FILE
+    echo "You are running with root access" 
 fi
 
 # validate functions takes input as exit status, what command they tried to install
 VALIDATE(){
     if [ $1 -eq 0 ]
     then
-        echo -e "Installing $2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
+        echo -e "Installing $2 is ... $G SUCCESS $N"
     else
-        echo -e "Installing $2 is ... $R FAILURE $N" | tee -a $LOG_FILE
+        echo -e "Installing $2 is ... $R FAILURE $N" 
         exit 1
     fi
 }
@@ -38,10 +38,10 @@ do
     dnf list installed $package &>>$LOG_FILE
     if [ $? -ne 0 ]
     then
-        echo "$package is not installed... going to install it" | tee -a $LOG_FILE
+        echo "$package is not installed... going to install it" 
         dnf install $package -y &>>$LOG_FILE
         VALIDATE $? "$package"
     else
-        echo -e "Nothing to do $package... $Y already installed $N" | tee -a $LOG_FILE
+        echo -e "Nothing to do $package... $Y already installed $N" 
     fi
 done
