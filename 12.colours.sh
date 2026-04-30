44b03f82a6668384ac192739ee4a59720b42c8f4
meharsai12 <meharsairavikanti@gmail.com>
2026-04-30 12:40:40 +0530

    Args through

---
+++ 12.colours.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+echo  -e "\e[31m  Hello world \e[om"
+echo  -e "\e[32m  Hello world \e[om"
+echo  -e "\e[33m  Hello world \e[om"
+echo  -e "\e[34m  Hello world \e[om"
+echo  -e "\e[35m  Hello world \e[om"
+echo  -e "\e[36m  Hello world \e[om"
+echo  -e "\e[37m  Hello world \e[om"
+
+echo "Hello world with no colours"
